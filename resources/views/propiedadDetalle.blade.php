6504fdc707dc181f7283f872c419b652a50242e7
AngelHdezDev <angeldev20340@gmail.com>
2026-05-21 09:35:21 -0600

    Mostrar la ubicacion en detalles

--- resources/views/propiedadDetalle.blade.php
+++ resources/views/propiedadDetalle.blade.php
@@ -123,7 +123,10 @@
                             <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
                             <circle cx="12" cy="10" r="3" />
                         </svg>
-                        {{ Str::limit($property->title, 40) }}
+                        {{ Str::limit($property->address, 40) }}
+                        {{ Str::limit($property->neighborhood, 40) }},
+                        {{ Str::limit($property->city, 40) }},
+                        {{ Str::limit($property->state, 40) }}
                     </div>
                     <div class="quick-stats">
                         <div class="stat-item">
